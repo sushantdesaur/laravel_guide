12b284b50dfedb865e7a9dfb1f6e2c054976b66e
sushantdesaur <desaursushant@gmail.com>
2021-11-19 10:55:37 +0530

    updated profiles controller followers & following

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -20,8 +20,19 @@ class ProfilesController extends Controller
                 return $user->posts->count();
             });
             
-        $followersCount = $user->profile->followers->count();
-        $followingCount = $user->following->count();
+        $followersCount = Cache::remember(
+            'count.followers.'. $user->id,
+             now()->addSeconds(30), 
+             function() use($user) {
+                 return $user->profile->followers->count();
+            });
+        
+        $followingCount = Cache::remember(
+            'count.following.'. $user->id,
+             now()->addSeconds(30), 
+             function() use($user) {
+                return $user->following->count();
+             });
 
         return view('profiles.index', compact('user', 'follows', 'postCount', 'followersCount', 'followingCount' ));
     }
