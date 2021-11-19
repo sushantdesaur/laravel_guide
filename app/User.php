fd31714d9915b1765a2e279af1047abff7c82371
sushantdesaur <desaursushant@gmail.com>
2021-11-19 13:51:49 +0530

    added email connection to .env and renamed app

--- app/User.php
+++ app/User.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+use App\Mail\NewUserWelcomeMail;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Mail;
+
 
 class User extends Authenticatable
 {
@@ -44,6 +47,8 @@ class User extends Authenticatable
             $user->profile()->create([
                 'title' => $user->username,
             ]);
+
+            Mail::to($user->email)->send(new NewUserWelcomeMail());
         });
     }
 
